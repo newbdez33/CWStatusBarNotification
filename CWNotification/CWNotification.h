0b62fad5bb1333a4fcb6a4876a751762159c6c0c
Jacky <newbdez33@gmail.com>
2016-09-17 14:36:17 +0800

    add share scheme to support carthage

---
+++ CWNotification/CWNotification.h
@@ -0,0 +1,19 @@
+//
+//  CWNotification.h
+//  CWNotification
+//
+//  Created by JackyZ on 17/9/2016.
+//  Copyright © 2016 Cezary Wojcik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CWNotification.
+FOUNDATION_EXPORT double CWNotificationVersionNumber;
+
+//! Project version string for CWNotification.
+FOUNDATION_EXPORT const unsigned char CWNotificationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CWNotification/PublicHeader.h>
+
+
